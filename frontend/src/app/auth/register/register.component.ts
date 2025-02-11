bb3517fd2f03f5ec00a4c8531068fe4757dec7ed
bonolo98 <bonolosolomon9846@gmail.com>
2025-02-12 00:00:18 +0200

    login CSS

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-register',
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, RouterLink],
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.css']
 })
