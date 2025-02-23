62ff2bc6add5a4586213af476c2e49e34ae38de1
bonolo98 <bonolosolomon9846@gmail.com>
2025-02-23 18:59:42 +0200

    all cors allowed

--- backend/server.js
+++ backend/server.js
@@ -16,23 +16,33 @@ app.use(bodyParser.json());
 app.use(logger);
 
 
-const allowedOrigins = [
-  'http://localhost:4200',
-  'https://ecommerce-9aaslz4fy-bonolos-projects-1c1373ab.vercel.app',
-  'https://ecommerce-app-bonolos-projects-1c1373ab.vercel.app'
-];
+// const allowedOrigins = [
+//   'http://localhost:4200',
+//   'https://ecommerce-9aaslz4fy-bonolos-projects-1c1373ab.vercel.app',
+//   'https://ecommerce-app-bonolos-projects-1c1373ab.vercel.app'
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: 'GET, POST, PUT, DELETE',
+//   allowedHeaders: 'Content-Type, Authorization'
+// }));
+
+
+const cors = require('cors');
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*', // Temporarily allow all origins for testing
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type, Authorization'
 }));
+
 
 
 // Routes
